e61756d932085b9468adaad4f6afff6c50bdbd98
Nag Varun Chunduru <nchunduru@hearsaycorp.com>
2016-04-06 14:15:33 -0700

    Added task for images.

---
+++ gulp/tasks/images.js
@@ -0,0 +1,8 @@
+'use strict';
+var gulp = require('gulp');
+var config = require('../config').images;
+
+gulp.task('images', function() {
+  return gulp.src(config.src)
+    .pipe(gulp.dest(config.dest));
+});
