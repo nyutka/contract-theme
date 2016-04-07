fb5094529affb83fb228e6c8cb5e36608a2c309b
Nag Varun Chunduru <nchunduru@hearsaycorp.com>
2016-04-07 11:49:29 -0700

    Added images to watch task,

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,5 +5,6 @@ var config = require('../config');
 gulp.task('watch', ['browserSync'], function() {
   gulp.watch(config.css.src, ['css:custom']);
   gulp.watch(config.html.src, ['html']);
+  gulp.watch(config.images.src, ['images']);
   gulp.watch(config.js.all, ['webpack']);
 });
